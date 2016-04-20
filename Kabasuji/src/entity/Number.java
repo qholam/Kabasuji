82b46a7c4592d45ea968f81c9851d474febc2ff1
qholam <qholam@wpi.edu>
2016-04-20 18:42:19 -0400

    Started work on PieceView. Change field of color from String to Color in Number class.

--- Kabasuji/src/entity/Number.java
+++ Kabasuji/src/entity/Number.java
@@ -1,21 +1,21 @@
 package entity;
 
+import java.awt.Color;
+
 public class Number {
 	//number of the tile and its color
 	int number;
-	String color;
+	Color color;
 	
 	//parameters for this class
 	private final int maxNumber = 6;
 	private final int minNumber = 1;
-	
 	/**
 	 * 
 	 * @param n number 
-	 * @param c
 	 * @throws Exception 
 	 */
-	public Number(int n, String c){
+	public Number(int n, Color c){
 		//makes sure that the 
 		try{
 			if(n > maxNumber || n < minNumber)
--- Kabasuji/src/view/PieceView.java
+++ Kabasuji/src/view/PieceView.java
@@ -2,8 +2,17 @@ package view;
 
 import javax.swing.JPanel;
 
+import entity.Piece;
+
 public class PieceView extends JPanel {
-	public PieceView() {
+	//Piece that this view represents
+	Piece piece;
+	
+	public PieceView(Piece p) {
+		piece = p;
+		
+		this.setSize(100, 100);
+		setLayout(null);
 	}
 
 }
