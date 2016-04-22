22e3f45cbac44e2a8abf9e75e5dc6345b308105d
Calvin <Calvin@172.31.99.250>
2016-04-22 10:33:17 -0400

    Added comments for all level classes

--- Kabasuji/src/entity/LightningLevel.java
+++ Kabasuji/src/entity/LightningLevel.java
@@ -2,18 +2,23 @@ package entity;
 
 import move.IMove;
 
+/**
+ * Constructor for a lightning level. Extends level.
+ * @author Calvin
+ *
+ */
 public class LightningLevel extends Level{
 
 	int timeRemaining;
 	
 	/**
 	 * constructor for a puzzle level
-	 * @param timeRemaining
-	 * @param board
-	 * @param bullpen
-	 * @param isUnlocked
-	 * @param levelNum
-	 * @param stars
+	 * @param timeRemaining The amount of tie remaining for the level.
+	 * @param board The board on which the level is played on.
+	 * @param bullpen Bullpen where the piece tiles will come from.
+ 	 * @param isUnlocked Boolean that determines if the level is unlocked.
+	 * @param levelNum Int the level number that this is.
+	 * @param stars The number of stars that the player has earned on this level.
 	 */
 	public LightningLevel(int timeRemaining, Board board,Bullpen bullpen, boolean isUnlocked, int levelNum, int stars){
 		super(board, bullpen, isUnlocked, stars, stars);
@@ -21,7 +26,7 @@ public class LightningLevel extends Level{
 	}
 	
 	/**
-	 * 
+	 * Checks whether or not the level has been won.
 	 * @return boolean determining if the level has been won
 	 */
 	public boolean hasWon(){
@@ -30,16 +35,15 @@ public class LightningLevel extends Level{
 	}
 	
 	/** 
-	 * 
+	 * Checks if move has been made.
 	 * @return boolean determining if the move was made
 	 */
 	public boolean doMove(IMove move){
-		
-		
-		return false;
+		return move.doMove();
 	}
 
 	/**
+	 * Gets the remaining time on the level.
 	 * @return timeRemaining
 	 */
 	public int getTimeRemaining(){
--- Kabasuji/src/entity/PuzzleLevel.java
+++ Kabasuji/src/entity/PuzzleLevel.java
@@ -2,17 +2,22 @@ package entity;
 
 import move.IMove;
 
+/**
+ * Constructor for a puzzle level. Extends level.
+ * @author Calvin
+ *
+ */
 public class PuzzleLevel extends Level {
 	int movesRemaining;
 	
 	/**
 	 * constructor for a LightningLevel
-	 * @param movesRemaining
-	 * @param board
-	 * @param bullpen
-	 * @param isUnlocked
-	 * @param levelNum
-	 * @param stars
+	 * @param movesRemaining The number of moves remaining for the player.
+	 * @param board The board on which the level is played on.
+	 * @param bullpen Bullpen where the piece tiles will come from.
+ 	 * @param isUnlocked Boolean that determines if the level is unlocked.
+	 * @param levelNum Int the level number that this is.
+	 * @param stars The number of stars that the player has earned on this level.
 	 */
 	public PuzzleLevel(int movesRemaining, Board board,Bullpen bullpen, boolean isUnlocked, int levelNum, int stars){
 		super(board, bullpen, isUnlocked, movesRemaining, movesRemaining);
@@ -20,7 +25,7 @@ public class PuzzleLevel extends Level {
 	}
 	
 	/**
-	 * 
+	 * Checks whether or not the level has been won.
 	 * @return boolean determining if the level has been won
 	 */
 	public boolean hasWon(){
@@ -29,14 +34,15 @@ public class PuzzleLevel extends Level {
 	}
 	
 	/** 
-	 * 
+	 * Checks if move has been made.
 	 * @return boolean determining if the move was made
 	 */
 	public boolean doMove(IMove move){
-		return false;
+		return move.doMove();
 	}
 	
 	/**
+	 * Checks the number of move remaining available. 
 	 * @return movesRemaining
 	 */
 	public int getMovesRemaining(){
